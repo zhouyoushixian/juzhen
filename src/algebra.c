1a0b403ae90389ac4a3759909e7c0e6790450abc
test <3402502077@qq.com>
2024-05-07 23:19:58 +0800

    TRY

--- src/algebra.c
+++ src/algebra.c
@@ -2,7 +2,7 @@
  * @Author: test 3402502077@qq.com
  * @Date: 2024-05-06 21:48:46
  * @LastEditors: test 3402502077@qq.com
- * @LastEditTime: 2024-05-07 23:05:09
+ * @LastEditTime: 2024-05-07 23:19:53
  * @FilePath: \juzhen\src\algebra.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -267,6 +267,8 @@ int rank_matrix(Matrix a)
     // ToDo
     return 0;
 }
+
+
 
 double trace_matrix(Matrix a)
 {
